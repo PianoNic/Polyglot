8e415ce7988230d3070f43ae6cb4a743806aae70
PianoNic <79938743+Pianonic@users.noreply.github.com>
2026-04-23 17:36:44 +0200

    Initial commit: Polyglot project with clean architecture

---
+++ src/Polyglot.API/Program.cs
@@ -0,0 +1,30 @@
+using System.Text.Json.Serialization;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+builder.Services.AddControllers().AddJsonOptions(options => options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/openapi/v1.json", "v1");
+    });
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ src/Polyglot.Infrastructure/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace Polyglot.Infrastructure
+{
+    public class Class1
+    {
+
+    }
+}
