4c332ba786bd35254fd5f640dd10e94b57e87045
PianoNic <79938743+PianoNic@users.noreply.github.com>
2026-05-03 13:09:23 +0200

    Added Propper Sidenav

--- src/Polyglot.Frontend/src/app/app.ts
+++ src/Polyglot.Frontend/src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HlmSidebarImports } from '@spartan-ng/helm/sidebar';
+import { Sidenav } from './sidenav/sidenav';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HlmSidebarImports, Sidenav],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
